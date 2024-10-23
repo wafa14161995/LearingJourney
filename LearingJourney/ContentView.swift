cbc353632c93105772416ca20eccdc5fe0c133db
wafa14161995 <Wafa14161995@gmail.com>
2024-10-23 16:30:37 +0300

    Initial Commit

---
+++ LearingJourney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearingJourney
+//
+//  Created by Wafa Awad  on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearingJourney/LearingJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingJourneyApp.swift
+//  LearingJourney
+//
+//  Created by Wafa Awad  on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearingJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
